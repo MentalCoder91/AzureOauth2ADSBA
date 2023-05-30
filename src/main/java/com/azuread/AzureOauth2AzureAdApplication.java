e09e37204aae048f59138c0040862df02ed511f2
Anish <anish.alwekar@gmail.com>
2023-05-30 18:20:25 +0530

    commit-1

---
+++ src/main/java/com/azuread/AzureOauth2AzureAdApplication.java
@@ -0,0 +1,13 @@
+package com.azuread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureOauth2AzureAdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureOauth2AzureAdApplication.class, args);
+	}
+
+}
